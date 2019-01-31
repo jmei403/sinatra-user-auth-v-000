e87e8dacd0fd129f273ba6d05a569c7bf7b0c979
Jason Mei <jason.mei403@gmail.com>
2019-01-31 03:11:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,13 +29,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    # @user = User.find_by(email: params[:email], password: params[:password])
-    # if @user
-    #   session[:user_id] = @user.id
-    #   redirect '/users/home'
-    # end
-    # redirect '/sessions/login'
-    puts params
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/users/home'
+    end
+    redirect '/sessions/login'
   end
 
   get '/sessions/logout' do
